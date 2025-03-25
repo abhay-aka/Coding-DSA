d2feea6f8a82fa253d164ffc947fc5f641da46bd
abhay-aka <67103822+abhay-aka@users.noreply.github.com>
2025-03-26 00:49:28 +0530

    Time: 158 ms (58.42%) | Memory: 236 MB (47.27%) - LeetSync

---
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int merge(vector<pair<int, int>> x) {
+        vector<pair<int, int>> merged;
+        for (auto it: x) {
+            if (merged.size() == 0 || merged[merged.size() - 1].second <= it.first) {
+                merged.push_back(it);
+            } else {
+                merged[merged.size() - 1].second = max(merged[merged.size() - 1].second, it.second);
+            }
+        }
+        return merged.size();
+    }
+    bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
+        vector<pair<int, int>> x, y;
+        for (auto it: rectangles) {
+            int start_x = it[0], end_x = it[2];
+            int start_y = it[1], end_y = it[3];
+            x.push_back({start_x, end_x});
+            y.push_back({start_y, end_y});
+        }
+        sort(x.begin(), x.end());
+        sort(y.begin(), y.end());
+        // for (auto it: x) {
+        //     cout<<it.first<<" "<<it.second<<", ";
+        // }
+        // cout<<endl;
+        // for (auto it: y) {
+        //     cout<<it.first<<" "<<it.second<<", ";
+        // }
+        // cout<<endl;
+        // return true;
+        int ansx = merge(x);
+        int ansy = merge(y);
+        cout<<ansx<<" "<<ansy<<" "<<endl;
+        if (ansx > 2 || ansy > 2) return true;
+        return false;
+
+    }
+};
